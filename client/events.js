1962c06c954056fc8dc468c531770a0d10d5e88b
Angle <Ribbonwing@gmail.com>
2016-10-06 15:56:46 -0600

    Replaced all coffescript with javascript.

---
+++ client/events.js
@@ -0,0 +1,11 @@
+Template.layout.events({
+  'click .button-users': function() {
+    return Router.go('/forum/users');
+  },
+  'click .button-home': function() {
+    return Router.go('/forum');
+  },
+  'click #at-nav-button': function() {
+    return Router.go('/sign-in');
+  }
+});
